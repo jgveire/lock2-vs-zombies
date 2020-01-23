28cc58d17b0bf863b81de8ed3d36b8cf1ed3e9b2
Jorrit <jorrit@veire.com>
2020-01-23 12:24:09 +0100

    Merge

--- main.ts
+++ main.ts
@@ -1,26 +1,50 @@
+function giveIntroduction () {
+    game.setDialogFrame(img`
+. 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+4 4 1 1 1 1 1 1 1 1 1 1 1 4 4 . 
+4 1 1 4 4 4 4 4 4 4 4 4 1 1 4 . 
+4 1 4 4 1 1 1 1 1 1 1 4 4 1 4 . 
+4 1 4 1 1 1 1 1 1 1 1 1 4 1 4 . 
+4 1 4 1 1 1 1 1 1 1 1 1 4 1 4 . 
+4 1 4 1 1 1 1 1 1 1 1 1 4 1 4 . 
+4 1 4 1 1 1 1 1 1 1 1 1 4 1 4 . 
+4 1 4 1 1 1 1 1 1 1 1 1 4 1 4 . 
+4 1 4 1 1 1 1 1 1 1 1 1 4 1 4 . 
+4 1 4 1 1 1 1 1 1 1 1 1 4 1 4 . 
+4 1 4 4 1 1 1 1 1 1 1 4 4 1 4 . 
+4 1 1 4 4 4 4 4 4 4 4 4 1 1 4 . 
+4 4 1 1 1 1 1 1 1 1 1 1 1 4 4 . 
+. 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+. . . . . . . . . . . . . . . . 
+`)
+    showInstruction("lock2 vs zombies, press A to fire.")
+}
+function blinkLeds () {
+    light.setAll(0xff0000)
+    pause(100)
+    light.clear()
+    pause(50)
+    light.setAll(0xff0000)
+    pause(100)
+    light.clear()
+    pause(50)
+    light.setAll(0xff0000)
+    pause(100)
+    light.clear()
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     lock2.setImage(img`
-. . . . . e e e e e e e e . . . 
-. . . . e e e e e e e e e e . . 
-. . . e e e e e e e e e e e . . 
-. . e e e e e e e e e e e e . . 
-. . e e e e e d d d d e e e . . 
-. . e e e e d d d d d d . . . . 
-. e e e e d d d e e d d . . . . 
-e e e e d d d d e e d d . . . . 
-e e e e d d d d e e d d . . . . 
-e e e e d d d d d d 2 2 . . . . 
-e e e e d d d d d d 2 2 . . d d 
-. . a a a a a a d d a a a a d d 
-. . a a a a a a d d a a a a . . 
-. . a a d d a a a a a a a a . . 
-. . a a d d a a a a a a a a . . 
-. . a a a a a a 8 8 a a . . . . 
-. . a a a a a a 8 8 a a . . . . 
-. . a a a a a a a a a a . . . . 
-. . a a a a a a a a a a . . . . 
-. . a a a a a a 8 8 a a . . . . 
-. . a a a a a a 8 8 a a . . . . 
+. . e e e . . . 
+. e e e e e e . 
+. e e e d e e . 
+. e e d e d . . 
+e e d d e d . . 
+e e d d d 2 . d 
+. a a a d a a . 
+. a d a a a a . 
+. a a a 8 a . . 
+. a a a a a . . 
+. a a a 8 a . . 
 `)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
@@ -29,20 +53,36 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
     info.changeScoreBy(1)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    music.pewPew.play()
     fireball = sprites.createProjectileFromSprite(img`
-. . . . . . . . 
-. . 4 . 2 2 4 . 
-4 . . 2 4 2 2 2 
-. . 4 2 2 2 4 2 
-4 . . . 4 2 2 . 
-. . . . . . . . 
+. 4 . 4 2 . 
+4 . 4 2 4 2 
+. 4 . 4 2 . 
 `, lock2, 200, 0)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    lock2.destroy(effects.disintegrate, 500)
+    music.baDing.play()
     info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate, 500)
+    sprite.startEffect(effects.disintegrate, 500)
+    blinkLeds()
+    sprite.setImage(img`
+. d d d d . 
+. d e e d . 
+d d d d d . 
+d d d e d . 
+d d d e d . 
+8 8 8 d 2 . 
+4 4 4 8 4 d 
+4 4 4 4 4 . 
+4 4 4 4 . . 
+4 4 4 4 . . 
+`)
 })
+function showInstruction (text: string) {
+    game.showLongText(text, DialogLayout.Bottom)
+    music.baDing.play()
+    info.changeScoreBy(1)
+}
 let zombie: Sprite = null
 let fireball: Sprite = null
 let lock2: Sprite = null
@@ -168,55 +208,37 @@ f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 `)
-info.setLife(1)
+music.setVolume(4)
+info.setLife(3)
 info.setScore(0)
 lock2 = sprites.create(img`
-. . d d d d d d d . . . 
-. d d d d e e d d d . . 
-. d d d e e e e d d . . 
-d d d d d d d d d d . . 
-d d d d d d e d d d . . 
-d d d d d d e e d d . . 
-d d d d d d e e d d . . 
-d d d d d d d d d d . . 
-8 8 8 8 8 d d d 2 2 . . 
-8 8 8 8 8 8 d d 2 2 . . 
-4 4 4 4 8 8 8 4 4 4 d d 
-4 4 4 4 4 8 8 8 4 4 d d 
-4 4 4 4 4 4 8 8 4 4 . . 
-4 4 4 4 4 4 4 4 4 . . . 
-4 4 4 4 4 4 4 4 . . . . 
-4 4 4 4 4 4 4 4 . . . . 
-4 4 4 4 4 4 4 4 . . . . 
-4 4 4 4 4 4 4 4 . . . . 
-4 4 4 4 4 4 4 4 . . . . 
-4 4 4 4 4 4 4 4 . . . . 
+. d d d d . 
+. d e e d . 
+d d d d d . 
+d d d e d . 
+d d d e d . 
+8 8 8 d 2 . 
+4 4 4 8 4 d 
+4 4 4 4 4 . 
+4 4 4 4 . . 
+4 4 4 4 . . 
 `, SpriteKind.Player)
 lock2.setFlag(SpriteFlag.StayInScreen, true)
 controller.moveSprite(lock2, 200, 200)
+giveIntroduction()
 game.onUpdateInterval(500, function () {
     zombie = sprites.create(img`
-. . . . 6 6 6 6 6 . . . 
-. . . 6 f f 1 1 6 6 . . 
-. . 6 6 f f 1 1 6 6 6 . 
-. 6 6 6 1 1 1 1 6 6 6 . 
-6 6 6 6 1 1 1 1 6 6 6 . 
-1 1 6 6 6 6 6 6 6 6 6 . 
-1 1 6 6 6 6 6 6 6 6 . . 
-f f 6 6 2 2 6 6 e e . . 
-f f 6 6 2 2 6 6 e e e . 
-. . . . e e e e e e e e 
-. . . e e e e e e e e e 
-. . e e e e e e e e e e 
-. . e e e e e e e e e e 
-. . 6 6 e e e e e e 8 8 
-. . 6 6 e e e e e e 8 8 
-. . . . 8 8 . . 8 8 8 8 
-. . . . 8 8 . . 8 8 8 8 
-. . . . 8 8 . . . . 6 6 
-. . . . 8 8 . . . . 6 6 
-. . . e e e . . . e e e 
-. . e e e e . . e e e e 
+. . 6 6 . . 
+. 6 f 1 6 . 
+6 6 1 1 6 . 
+1 6 6 6 6 . 
+f 6 2 6 e . 
+. . e e e e 
+. e e e e e 
+. 6 e e e 8 
+. . 8 . 8 8 
+. . 8 . . 6 
+. e e . e e 
 `, SpriteKind.Enemy)
     zombie.setVelocity(-100, 0)
     zombie.setPosition(180, Math.randomRange(8, 112))
